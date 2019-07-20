fd7017022b581189c79317b41a174e95c1ac30f3
Liam Howell <liam@liam.codes>
2019-07-20 15:57:11 +0100

    fix: fix arrow direction for gold collection

--- src/js/functions/process-creature-death.js
+++ src/js/functions/process-creature-death.js
@@ -8,8 +8,8 @@ const processCreatureDeath = () => {
   Player.store.commit({ gold: Player.store.state.gold + creature.store.state.raw.gold, totalGold: Player.store.state.totalGold + creature.store.state.raw.gold });
   Player.store.commit({ kills: Player.store.state.kills + 1 });
   Dungeon.advance();
-  log(`* << Enemy ${capitalize(creature.store.state.raw.name)} succumbs to its wounds and perishes`, 'CREATURE_DEATH');
-  log(`* >> You recieve ${creature.store.state.raw.gold} <div class="tm-c-log__keyword">gold</div>`);
+  log(`* >> Enemy ${capitalize(creature.store.state.raw.name)} succumbs to its wounds and perishes`, 'CREATURE_DEATH');
+  log(`* << You recieve ${creature.store.state.raw.gold} <div class="tm-c-log__keyword">gold</div>`);
 };
 
 export { processCreatureDeath };
