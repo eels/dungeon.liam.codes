682930284d80f800b0def7adc7a48ec2a97efce9
Liam Howell <liam@liam.codes>
2021-11-11 20:12:31 +0000

    fix: prevent creature update event firing on checkpoints

--- src/events/handlers/custom-creature-update.js
+++ src/events/handlers/custom-creature-update.js
@@ -1,9 +1,14 @@
+import Dungeon from 'instances/Dungeon';
 import Creature from 'components/Creature';
 import bind from 'events/delegate/bind';
 import { CREATURE_UPDATE } from 'events/events';
 
 bind(CREATURE_UPDATE, 'body', function () {
-  document.querySelector('.tm-c-creature').outerHTML = Creature();
+  const creature = Dungeon.creatures[0];
+
+  if (creature !== undefined) {
+    document.querySelector('.tm-c-creature').outerHTML = Creature();
+  }
 });
 
 export default true;
