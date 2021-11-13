a64216cf634b6758f956cc25f60ab764a993447e
Liam Howell <liam@liam.codes>
2021-11-13 02:35:45 +0000

    feat: add animation to discard pile

--- src/functions/add-to-discard.js
+++ src/functions/add-to-discard.js
@@ -2,7 +2,13 @@ import nodize from 'utilities/nodize';
 
 export default function addToDiscard() {
   const pile = document.querySelector('.js-discard');
-  const card = nodize('<div class="tm-c-hand__card tm-c-card"></div>');
+
+  const card = nodize(`
+    <div class="tm-c-hand__discard-wrapper tm-c-hand__discard-wrapper--enter">
+      <div class="tm-c-hand__card tm-c-card"></div>
+    </div>
+  `);
+
   const cardRoot = card.querySelector('.tm-c-hand__card');
 
   if (pile.children.length === 4) {
@@ -10,12 +16,19 @@ export default function addToDiscard() {
   }
 
   const lastChild = pile.lastChild;
-  const lastChildTransform = lastChild && lastChild.style.transform;
-  const lastAngle = lastChild && parseFloat(lastChildTransform.match(/[-\d.]+/));
+  const lastChildCard = lastChild && lastChild.querySelector('.tm-c-hand__card');
+  const lastChildTransform = lastChildCard && lastChildCard.style.transform;
+  const lastAngle = lastChildCard && parseFloat(lastChildTransform.match(/[-\d.]+/));
   const axis = lastAngle < 0 ? 1 : -1;
   const degree = (Math.round(Math.random() * 6) + 6) * axis;
 
   cardRoot.style.transform = `rotate(${degree}deg)`;
 
   pile.appendChild(card);
+
+  setTimeout(() => {
+    const newChild = document.querySelector('.js-discard').lastChild;
+
+    newChild.classList.remove('tm-c-hand__discard-wrapper--enter');
+  }, 500);
 }
